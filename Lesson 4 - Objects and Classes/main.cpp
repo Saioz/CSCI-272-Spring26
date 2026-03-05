72d3f41630ded0b8a0c9a28385edd2cc615d088e
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-05 16:16:24 -0500

    main.cpp

---
+++ Lesson 4 - Objects and Classes/main.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+
+
+/*
+Name: Anthony Lin 
+Course: CSCI 272
+Lesson Number Lesson 4 
+Code Topic: Copy constructor Example
+Date: 3/3/2026
+
+*/
+
+
+
+int main()
+{
+Person person1;
+person1.introduce();
+//If I wanted to change it a little I could create constructors for it instead. Then I could set parameters instead of having to change it individually. person1.name person1.age person1.gender for instance. 
+    
+}
+
+// Reflection
+/*
+
+What you understood from this example
+• Any difficulties you faced
+• What you searched or researched online (if anything)
+• Whether you used AI (if yes, explain how)
+• What you learned from completing this code
+
+THIS IS FOR REFLECTION DELETE AFTERWARDS
+
+*/
+
+
